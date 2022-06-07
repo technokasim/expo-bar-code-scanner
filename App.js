38e3e5b8542af6c2358feb7f230f40897db93af8
technokasim <technokasim@gmail.com>
2022-06-07 23:07:02 +0400

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import BarcodeMask from 'react-native-barcode-mask';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RNCamera>
+        <BarcodeMask />
+    </RNCamera>
   );
 }
 
